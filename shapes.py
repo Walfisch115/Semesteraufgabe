0006dfca56aee3c0bde9b799df733b0aab4bfb8d
Walfisch115 <maxraeck12@gmail.com>
2022-05-11 14:36:51 +0200

    Änderung

---
+++ shapes.py
@@ -0,0 +1,185 @@
+import pygame
+import random
+
+from pygame.locals import *
+
+# spielfiguren
+# I, J, L, O, S, T und Z
+
+# bausteine rendern
+
+# name der shapes ist hier in der schleife noch falsch, ich hatte die nochmal umbenannt
+'''for h in range(len(shapes.shape_i)):
+    for w in range(len(shapes.shape_i[0])):
+        if shapes.shape_i[h][w] != 0:
+            pygame.draw.rect(window, (255, 0, 255), (w * 10, h * 10, 10, 10))'''
+
+
+def shape(name, rotation=0):
+
+    if name == 'i':
+
+        if rotation == 0 or rotation == 180:
+
+            shape_i = [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ]
+
+        elif rotation == 90 or rotation == 270:
+
+            shape_i = [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
+            ]
+
+    if name == 'j':
+
+        if rotation == 0:
+
+            shape_j = [
+                [1, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0]
+            ]
+
+        elif rotation == 90:
+
+            shape_j = [
+                [0, 1, 1],
+                [0, 1, 0],
+                [0, 1, 0]
+            ]
+
+        elif rotation == 180:
+
+            shape_j = [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 1]
+            ]
+
+        elif rotation == 270:
+
+            shape_j = [
+                [0, 1, 0],
+                [0, 1, 0],
+                [1, 1, 0]
+            ]
+
+    if name == 'l':
+
+        if rotation == 0:
+
+            shape_l = [
+                [0, 0, 1],
+                [1, 1, 1],
+                [0, 0, 0]
+            ]
+
+        elif rotation == 90:
+
+            shape_l = [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 1]
+            ]
+
+        elif rotation == 180:
+
+            shape_l = [
+                [0, 0, 0],
+                [1, 1, 1],
+                [1, 0, 0]
+            ]
+
+        elif rotation == 270:
+
+            shape_l = [
+                [1, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0]
+            ]
+
+    if name == 'o':
+
+        if rotation == 0 or rotation == 90 or rotation == 180 or rotation == 270:
+
+            shape_o = [
+                [1, 1],
+                [1, 1]
+            ]
+
+    if name == 's':
+
+        if rotation == 0 or rotation == 180:
+
+            shape_s = [
+                [0, 1, 1],
+                [1, 1, 0],
+                [0, 0, 0]
+            ]
+
+        elif rotation == 90 or rotation == 270:
+
+            shape_s = [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 0, 1]
+            ]
+
+    if name == 't':
+
+        if rotation == 0:
+
+            shape_t = [
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 0, 0]
+            ]
+
+        elif rotation == 90:
+
+            shape_t = [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 1, 0]
+            ]
+
+        elif rotation == 180:
+
+            shape_t = [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0]
+            ]
+
+        elif rotation == 270:
+
+            shape_t = [
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 1, 0]
+            ]
+
+    if name == 'z':
+
+        if rotation == 0 or rotation == 180:
+
+            shape_z = [
+                [1, 1, 0],
+                [0, 1, 1],
+                [0, 0, 0]
+            ]
+
+        elif rotation == 90 or rotation == 270:
+
+            shape_z = [
+                [0, 0, 1],
+                [0, 1, 1],
+                [0, 1, 0]
+            ]
--- tetris.py
+++ tetris.py
@@ -1,8 +1,45 @@
 import pygame
 import random
 
+import shapes
+
 from pygame.locals import *
 
 # pygame initialisieren
 pygame.init()
+
+# window erstellen
+width = 720
+height = 480
+window = pygame.display.set_mode((width, height))
+window.fill((0, 0, 0))
+pygame.display.set_caption('Ultimate Tetris')
+
+# farben
+RED = pygame.Color(255, 0, 0)
+GREEN = pygame.Color(0, 255, 0)
+BLUE = pygame.Color(0, 0, 255)
+YELLOW = pygame.Color(255, 255, 0)
+ORANGE = pygame.Color(255, 128, 0)
+LIGHT_BLUE = pygame.Color(0, 255, 255)
+PURPLE = pygame.Color(255, 0, 255)
+
+# spielfeld
+# größe 10 x 20
+
+keepGoing = True
+paint = False
+clock = pygame.time.Clock()
+
+# gameloop
+while keepGoing:
+
+    # fps
+    clock.tick(30)
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            keepGoing = False
+            break
 
